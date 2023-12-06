b9ccd6dc550dd41dbd5d5435908c171729efecc6
pthienquan1 <quan01011997123@gmail.com>
2023-12-06 15:21:32 +0700

    First load activity

---
+++ app/src/main/java/com/example/android3011/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.android3011
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
