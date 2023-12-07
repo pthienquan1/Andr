0246fd232a3cd06cc929daf7be07eb4cce1c1d27
pthienquan1 <quan01011997123@gmail.com>
2023-12-07 13:21:54 +0700

    signup activity

--- app/src/main/java/com/example/android3011/activity_signup.kt
+++ app/src/main/java/com/example/android3011/activity_signup.kt
@@ -1,13 +1,11 @@
 package com.example.android3011
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class activity_signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
     }
 }
