54c5e0ed9852a39c1a08efb6468fb780d1dea8e4
pthienquan1 <quan01011997123@gmail.com>
2023-12-07 11:55:04 +0700

    Login activity

---
+++ app/src/main/java/com/example/android3011/activity_signin.kt
@@ -0,0 +1,11 @@
+package com.example.android3011
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_signin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
